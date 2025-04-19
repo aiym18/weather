e3a5adffbf1e336217554374b0588d9a443766c0
맥북 <maegbug@maegbug-ui-MacBookAir.local>
2025-04-19 13:23:38 +0600

    finish

--- src/companents/Hero/index.js
+++ src/companents/Hero/index.js
@@ -28,23 +28,28 @@ const Hero = () => {
               type="button"
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             >
-              Default
+              Search
             </button>
           </div>
           <>
             {Object.keys(mainData).length ? (
               <div className="hero--cart">
-                <img src="https://openweathermap.org/img/w/$%7Bel.icon04d.png" alt='' />
+                <img
+                  src="https://openweathermap.org/img/w/$%7Bel.icon04d.png"
+                  alt=""
+                />
                 <h1>{mainData?.sys?.country}</h1>
                 <h1>{mainData?.name}</h1>
                 <h2>Tемпература: {mainData?.main?.temp}</h2>
                 <h2>{mainData?.description}</h2>
                 {mainData?.weather?.map((el) => (
                   <>
-                  <h2>{el?.description}</h2>
-                  <img width={200} src={`https://openweathermap.org/img/w/${el.icon}.png `} ></img>
+                    <h2>{el?.description}</h2>
+                    <img
+                      width={200}
+                      src={`https://openweathermap.org/img/w/${el.icon}.png `}
+                    ></img>
                   </>
-                  
                 ))}
               </div>
             ) : null}
