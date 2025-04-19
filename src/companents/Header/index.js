c31ac716ff0d8fca3f447687c17b0a2b59779677
맥북 <maegbug@maegbug-ui-MacBookAir.local>
2025-04-19 13:21:01 +0600

    finish

--- src/companents/Header/index.js
+++ src/companents/Header/index.js
@@ -47,7 +47,7 @@ const Header = () => {
                 class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
                 aria-current="page"
               >
-                Weater
+                Weather
               </Link>
             </li>
             <li>
